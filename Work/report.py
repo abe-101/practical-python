7679abd061fcedfbda340b02fa7ce286acfd1dde
Abe <abe@habet.dev>
2022-07-15 16:42:24 -0400

    add report

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,31 @@
 # report.py
 #
 # Exercise 2.4
+
+
+import csv
+
+def read_portfolio(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = {'name': row[0], 'shares': int(row[1]), 'price': float(row[2])}
+            portfolio.append(holding)
+
+    return portfolio
+
+def read_prices(filename):
+    prices = { }
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
+
