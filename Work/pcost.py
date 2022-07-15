85de4d1398d4f5d8399a09c25cd70da45d7c98e4
Abe <abe@habet.dev>
2022-07-15 13:16:04 -0400

    add cmd-line aurguments option

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,6 +2,7 @@
 #
 # Exercise 1.27
 import csv
+import sys
 
 def portfolio_cost(filename):
     total_cost = 0.0
@@ -15,7 +16,11 @@ def portfolio_cost(filename):
                 print("Couldn't parse", row)
     return total_cost
 
-cost = portfolio_cost('Data/portfolio.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
 
 
