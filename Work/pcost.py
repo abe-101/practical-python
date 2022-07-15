03868e5d38e86eda7a7eb88127220cde2a1fa647
Abe <abe@habet.dev>
2022-07-14 22:44:30 -0400

    add pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0.0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        _, shares, price = line.split(',')
+        total_cost += int(shares) * float(price)
+
+print(f'Total cost {total_cost}')
+
+
