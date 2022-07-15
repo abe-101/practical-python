df11baba1be1266c1e506c91db64717e2e1c46ed
Abe <abe@habet.dev>
2022-07-15 13:06:29 -0400

    catch error

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        for line in f:
+            try:
+                _, shares, price = line.split(',')
+                total_cost += int(shares) * float(price)
+            except ValueError:
+                print("Couldn't parse", line)
+    return total_cost
 
-total_cost = 0.0
-
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        _, shares, price = line.split(',')
-        total_cost += int(shares) * float(price)
-
-print(f'Total cost {total_cost}')
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
 
 
